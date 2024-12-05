446902bbda7c545aeced6c6acd4343ec8df793a2
cuaylkls <adamdelves57489@sccode.com>
2024-12-05 20:08:39 +0000

    Day 1 part 2

--- day1.py
+++ day1.py
@@ -3,7 +3,10 @@ import re
 # initilise lists and return vars
 loc1 = []
 loc2 = []
+loc2_dict = {}
+
 result = 0
+similarity_score = 0
 
 with open("inputs/day1.txt") as f:
     lines = f.readlines()
@@ -11,22 +14,38 @@ with open("inputs/day1.txt") as f:
     for line in lines:
         # read list items
         locations = re.split(r"\s+", line.strip())
-        loc1.append(int(locations[0]))
-        loc2.append(int(locations[1]))
+
+        loc1_number = int(locations[0])
+        loc2_number = int(locations[1])
+        loc1.append(loc1_number)
+        loc2.append(loc2_number)
+
+        # found a number in the first location which is not in the second
+        # initilise the number in the dict
+        if loc1_number not in loc2_dict:
+            loc2_dict[loc1_number] = 0
+
+        # found a number in the second location we haven't seen before,
+        # add to the list and initilise with 1
+        if loc2_number not in loc2_dict:
+            loc2_dict[loc2_number] = 1
+        else:
+            loc2_dict[loc2_number] += 1
 
 # sort both lists
 loc1.sort()
 loc2.sort()
 
 for x in range(len(loc1)):
-    # find the maximum and minimum locations
-    no1 = max(loc1[x], loc2[x])
-    no2 = min(loc1[x], loc2[x])
-
     # find the difference between the locations
-    result += no1 - no2
+    result += abs(loc1[x] - loc2[x])
+
+   # calculate the similarity score for the number and add to the similarity score
+    # variable
+    similarity_score += loc1[x] * loc2_dict[loc1[x]]
 
 print(f"Part 1: {result}")
+print(f"Part 2: {similarity_score}")
 
 
 
